5e92b30965e303a9f957ac11d3c72582190e20ed
matanr <matanr96@gmail.com>
2019-07-25 08:39:06 +0300

    deleted some not important imports from auto_encoder.py

--- auto_encoder.py
+++ auto_encoder.py
@@ -1,15 +1,3 @@
-import os
-import numpy as np
-import cv2 as cv
-import matplotlib.pyplot as plt
-import matplotlib.cm as cm
-import matplotlib.colors as clrs
-import matplotlib.colorbar as clbr
-from scipy import ndimage
-import scipy.spatial.distance as dist
-from pyod.models.auto_encoder import AutoEncoder
-from pyod.utils.data import evaluate_print
-from smallestenclosingcircle import make_circle
 from load_data import *
 
 
