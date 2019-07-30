07ad97607a2a79f2125e2d380ac95e3de17bd2bb
matanr <matanr96@gmail.com>
2019-07-30 18:47:04 +0300

    dimension are 300x300

--- auto_encoder.py
+++ auto_encoder.py
@@ -14,8 +14,8 @@ import numpy as np
 #                       input_shape=(HEIGHT, WIDTH, 3))
 # base_model = ResNet50(input_shape=(HEIGHT, WIDTH, 3))
 
-HEIGHT = 600
-WIDTH = 600
+HEIGHT = 300
+WIDTH = 300
 BATCH_SIZE = 64
 EPOCHS_NUM = 1000
 
