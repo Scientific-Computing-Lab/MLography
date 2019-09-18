b29f903881368e21d66391db665586e4c6cdaee9
matanr <matanr@usersrv.rcl_hpc>
2019-09-18 22:52:20 +0300

    added shape anomaly functionality

---
+++ data_preparation.py
@@ -0,0 +1,171 @@
+import numpy as np
+import cv2 as cv
+
+def normalize_circle_boxes(img, markers, imp_boxes, areas, indices, scores, dr_max=300, dc_max=300,
+                           write_to_files=False, scan_name="", number_of_impurities_to_write=None, write_circles=True,
+                           write_all=True, dest_path="./data/all_regularized_impurities_anomaly/"):
+    """
+    normalize the impurity images into a fixed size, and standardize the impurities to be in the center.
+    :param img: original image
+    :param markers: the markers from get_markers function
+    :param imp_boxes: the bounding boxes of the impurities
+    :param areas: the ares of the impurities
+    :param indices: the indices of the significant impurities (the ones with a not too-small size)
+    :param scores: the anomaly scores of the impurities. used for writing the score to the name of the file
+    :param dr_max: optional, the maximum difference of rows (height) of the impurity that is tolerated
+    :param dc_max: optional, the maximum difference of columns (width) of the impurity that is tolerated
+    :param write_to_files: True - write the impurities to files, False - do not write.
+    :param scan_name: optional - the name of the scan.
+    :param number_of_impurities_to_write: optional - maximum impurities allowed to be written
+    :param write_circles: True - if writing impurities that are closed to circles is desired.
+                          False - for writing anomaly impurities (not closed to circles)
+    :param write_all: True only if writing all significant impurities from a specific scan is intended
+    :param dest_path: The base destination path of the directory in which the output should be written to
+    """
+    if dr_max is None or dc_max is None:
+        dr_max = 0
+        dc_max = 0
+
+        for impurity in indices:
+            rmin, rmax, cmin, cmax = imp_boxes[impurity]
+            dr = rmax - rmin
+            dc = cmax - cmin
+            if dr > dr_max:
+                dr_max = dr
+            if dc > dc_max:
+                dc_max = dc
+
+    dr_max = int(dr_max * 2)
+    dc_max = int(dc_max * 2)
+
+    too_big_counter = 0
+
+    print("Starting to write normalized impurities")
+    normalized = np.zeros(imp_boxes.shape[0])
+
+    number_of_written_impurities = 0
+    for impurity in indices:
+        # take only circle impurities OR
+        # take only non-circle impurities as anomalies OR
+        # take all significant impurities
+        if (write_circles and scores[impurity] <= 0.2 and areas[impurity] > 50) or \
+                (not write_circles and scores[impurity] > 0.6 and areas[impurity] > 50) or write_all:
+            rmin, rmax, cmin, cmax = imp_boxes[impurity]
+            dr = int(rmax - rmin)
+            dc = int(cmax - cmin)
+            if 2*dr > dr_max or 2*dc > dc_max:
+                # skip too big impurities
+                too_big_counter += 1
+                continue
+
+            pad_r = int((dr_max - dr) // 2)
+            pad_c = int((dc_max - dc) // 2)
+
+            blank_image = np.zeros((dr_max, dc_max, 3), np.uint8)
+            blank_image[:, :] = (255, 255, 255)
+
+            image = np.zeros(img.shape, np.uint8)
+            image[:, :] = (255, 255, 255)
+            image[markers == impurity+2] = img[markers == impurity+2]
+            blank_image[pad_r:pad_r+dr, pad_c:pad_c+dc] = image[int(rmin):int(rmax), int(cmin):int(cmax)]
+            # normalized[impurity] = blank_image
+            if write_to_files:
+                string_score = str(scores[impurity])
+                string_score.replace('.', '_')
+                cv.imwrite(dest_path + string_score +
+                           scan_name + "_impurity_" + str(impurity) +".png", blank_image)
+
+            number_of_written_impurities += 1
+            if number_of_impurities_to_write is not None and \
+                    number_of_written_impurities >= number_of_impurities_to_write:
+                return normalized
+        print ("too big impurites: " + str(too_big_counter))
+    return normalized
+
+
+def rescale_and_write_normalized_impurity(img, markers, imp_boxes, areas, indices, scores, height=100, width=100,
+                                          proportion_impurity_of_image=0.8,
+                                          scan_name="",
+                                          dest_path_normal="./data/rescaled/normal/",
+                                          dest_path_anomaly="./data/rescaled/anomaly/",
+                                          write_all=False,
+                                          dest_path_all="./data/rescaled/all/"):
+    """
+    rescale the impurity images into a fixed size, and standardize the impurities to be in the center.
+    :param img: original image
+    :param markers: the markers from get_markers function
+    :param imp_boxes: the bounding boxes of the impurities
+    :param areas: the ares of the impurities
+    :param indices: the indices of the significant impurities (the ones with a not too-small size)
+    :param scores: the anomaly scores of the impurities. used for writing the score to the name of the file
+    :param dr_max: optional, the maximum difference of rows (height) of the impurity that is tolerated
+    :param dc_max: optional, the maximum difference of columns (width) of the impurity that is tolerated
+    :param write_to_files: True - write the impurities to files, False - do not write.
+    :param scan_name: optional - the name of the scan.
+    :param number_of_impurities_to_write: optional - maximum impurities allowed to be written
+    :param write_circles: True - if writing impurities that are closed to circles is desired.
+                          False - for writing anomaly impurities (not closed to circles)
+    :param write_all: True only if writing all significant impurities from a specific scan is intended
+    :param dest_path: The base destination path of the directory in which the output should be written to
+    """
+
+    print("Starting to write normalized impurities of ", scan_name)
+    normalized = np.zeros(imp_boxes.shape[0])
+
+    number_of_written_impurities = 0
+    for impurity in indices:
+        if impurity == 717:
+            print("in imp 717")
+        # take only circle impurities OR
+        # take only non-circle impurities as anomalies OR
+        # take all significant impurities
+
+        rmin, rmax, cmin, cmax = imp_boxes[impurity]
+        dr = int(rmax - rmin)
+        dc = int(cmax - cmin)
+        blank_image = np.zeros((dr, dc, 3), np.uint8)
+        blank_image[:, :] = (255, 255, 255)
+
+        image = np.zeros(img.shape, np.uint8)
+        image[:, :] = (255, 255, 255)
+        # take only the indices of the impurity
+        image[markers == impurity + 2] = img[markers == impurity + 2]
+        # take the bounding box of the impurity
+        blank_image[:, :] = image[int(rmin):int(rmax), int(cmin):int(cmax)]
+        # blank_image = blank_image / 255.0  # conversion for opencv images
+        scale_factor_r = height * proportion_impurity_of_image / dr
+        scale_factor_c = width * proportion_impurity_of_image / dc
+        scale_factor = min(scale_factor_r, scale_factor_c)
+
+        h = int(dr * scale_factor)
+        w = int(dc * scale_factor)
+        dim = (w, h)
+
+        if h == 0 or w == 0:
+            continue
+
+        scaled_image = cv.resize(blank_image, dim)
+
+        normalized_scaled_image = np.zeros((height, width, 3), np.uint8)
+        normalized_scaled_image[:, :] = (255, 255, 255)
+
+        pad_r = int((height - h) // 2)
+        pad_c = int((width - w) // 2)
+        normalized_scaled_image[pad_r:pad_r + h, pad_c:pad_c + w] = scaled_image[:, :]
+
+        string_score = str(scores[impurity])
+        string_score.replace('.', '_')
+        # normal impurity
+        if write_all is False:
+            if scores[impurity] <= 0.3 and areas[impurity] > 50:
+                cv.imwrite(dest_path_normal + string_score +
+                           scan_name + "_impurity_" + str(impurity) + ".png", normalized_scaled_image)
+                number_of_written_impurities += 1
+            # anomalous impurity
+            elif scores[impurity] > 0.55 and areas[impurity] > 50:
+                cv.imwrite(dest_path_anomaly + string_score +
+                           scan_name + "_impurity_" + str(impurity) + ".png", normalized_scaled_image)
+                number_of_written_impurities += 1
+        else:
+            cv.imwrite(dest_path_all + string_score +
+                       scan_name + "_impurity_" + str(impurity) + ".png", normalized_scaled_image)
---
+++ use_model.py
@@ -0,0 +1,175 @@
+from keras.models import load_model
+from keras.preprocessing import image
+import matplotlib.pyplot as plt
+import numpy as np
+from keras.preprocessing.image import ImageDataGenerator
+from keras.preprocessing.image import array_to_img
+from keras.preprocessing.image import save_img
+from sklearn.metrics import mean_squared_error
+from PIL import Image
+import os
+import re
+import cv2 as cv
+
+
+def load_image(img_path, height=100, width=100, show=True):
+
+    img = image.load_img(img_path, target_size=(height, width), color_mode = "grayscale")
+    # img = image.load_img(img_path, target_size=(height, width))
+    img_tensor = image.img_to_array(img)                    # (height, width, channels)
+    img_tensor = np.expand_dims(img_tensor, axis=0)         # (1, height, width, channels), add a dimension because the model expects this shape: (batch_size, height, width, channels)
+    img_tensor /= 255.                                      # imshow expects values in the range [0, 1]
+
+    # if show:
+    #     plt.imshow(img_tensor[0])
+    #     plt.axis('off')
+    #     plt.show()
+
+    return img_tensor
+
+
+def fixed_generator_none(generator):
+    """
+    """
+    for batch in generator:
+        yield (batch, batch)
+
+
+def test_2_impurities(model_name='./model.h5', height=100, width=100):
+    model = load_model(model_name)
+
+    normal_imp = "./data/test_rescaled/normal/test/0.151907915704087scan3tag-48_impurity_242.png"
+
+    anomaly_imp = "./data/test_rescaled/anomaly/test/0.624159885095173scan2tag-34_impurity_875.png"
+
+    anomaly_imp2 = "./data/test_rescaled/anomaly/test/0.93742507727708scan1tag-47_impurity_1056.png"
+
+    anomaly_imp717 = "./data/test_scan1tag-47/scan1tag-47/0.8692158152501969scan1tag-47_impurity_717.png"
+
+    model.compile(loss="mean_squared_error",
+                  optimizer='rmsprop',
+                  metrics=['accuracy'])
+
+    # load a single image
+    new_image_anomaly = load_image(anomaly_imp, height, width)
+
+    new_image_anomaly2 = load_image(anomaly_imp2, height, width)
+
+    new_image_anomaly717 = load_image(anomaly_imp717, height, width)
+
+    new_image_normal = load_image(normal_imp, height, width)
+    print("loaded images")
+
+    # check prediction
+    pred_a = model.predict(new_image_anomaly)
+    print("predicted anomaly")
+
+    pred_a2 = model.predict(new_image_anomaly2)
+    print("predicted line anomaly")
+
+    pred_a717 = model.predict(new_image_anomaly717)
+    print("predicted anomaly 717")
+
+    pred_n = model.predict(new_image_normal)
+    print("predicted normal")
+
+    # print prediction
+    # print('Predicted anomaly:', pred_a)
+    # print('Predicted normal:', pred_n)
+
+    save_img('reconstructed_anomaly.jpg', pred_a[0])
+    save_img('reconstructed_anomaly_line.jpg', pred_a2[0])
+    save_img('reconstructed_anomaly_717.jpg', pred_a717[0])
+
+    save_img('reconstructed_normal.jpg', pred_n[0])
+    print("saved images")
+
+    fig, ((ax11, ax12, ax13, ax14), (ax21, ax22, ax23, ax24)) = plt.subplots(2, 4)
+
+    ax11.imshow(cv.imread(normal_imp))
+    ax12.imshow(cv.imread(anomaly_imp))
+    ax13.imshow(cv.imread(anomaly_imp2))
+    ax14.imshow(cv.imread(anomaly_imp717))
+
+    ax21.imshow(np.expand_dims(pred_n[0], axis=0))
+    ax22.imshow(np.expand_dims(pred_a[0], axis=0))
+    ax23.imshow(np.expand_dims(pred_a2[0], axis=0))
+    ax24.imshow(np.expand_dims(pred_a717[0], axis=0))
+
+    plt.show()
+
+
+
+    # print("len anomal: " + str(pred_a.shape))
+    # print("len normal: " + str(pred_n.shape))
+    #
+    # # display reconstruction
+    # plt.figure(figsize=(1, 2))
+    #
+    # # display anomaly reconstruction
+    # ax = plt.subplot(1, 2, 1)
+    # plt.imshow(pred_a[0], cmap='gray')
+    #
+    # # display normal reconstruction
+    # ax = plt.subplot(1, 2, 2)
+    # plt.imshow(pred_n[0], cmap='gray')
+    # plt.show()
+    # plt.savefig('reconstruct.png')
+
+
+
+def get_score_from_prediction(input, prediction):
+    loss = mean_squared_error(input, prediction)
+    return loss ** 4
+
+
+def predict(path, impurities_num, model_name='./model_ae_extended.h5', height=100, width=100, BATCH_SIZE=64):
+    model = load_model(model_name)
+
+    model.compile(loss='mse',
+                  optimizer='rmsprop',
+                  metrics=['accuracy'])
+
+    datagen = ImageDataGenerator(rescale=1. / 255)
+    test_it = datagen.flow_from_directory(path, target_size=(height, width), class_mode=None,
+                                          batch_size=BATCH_SIZE,  color_mode='grayscale')
+    filenames = test_it.filenames
+    samples_num = len(filenames)
+    test_it.reset()
+    pred = model.predict_generator(fixed_generator_none(test_it), verbose=1, steps=samples_num/BATCH_SIZE)
+    # evaluated_loss = model.evaluate_generator(fixed_generator_none(test_it), verbose=1, steps=samples_num/BATCH_SIZE)
+    # print("filenames: ", filenames)
+    # print(pred)
+
+    # labels = (train_it.class_indices)
+    # labels = dict((v, k) for k, v in labels.items())
+    # predictions = [labels[k] for k in predicted_class_indices]
+
+    # print(filenames)
+    impurity_anomaly_shape_scores = np.full(impurities_num, np.infty)
+    # impurity_anomaly_shape_reconstruct_loss = np.full(impurities_num, np.infty)
+    for i in range(len(filenames)):
+        img_name = os.path.splitext(os.path.basename(filenames[i]))[0]
+        img_name = img_name[img_name.find("_impurity_"):]
+        imp_num = int(re.search(r'\d+', img_name).group())
+
+        # print("input path: " + path + filenames[i])
+        input_image = load_image(path + filenames[i])
+        # print("input dim:" + str(input_image.shape))
+        # print("pred dim:" + str(pred[i].shape))
+        impurity_anomaly_shape_scores[imp_num] = get_score_from_prediction(input_image[0,:,:,0], pred[i][:,:,0])
+        if imp_num == 717:
+            print("loss of impurity 717: "+str(impurity_anomaly_shape_scores[imp_num]))
+
+        # impurity_anomaly_shape_reconstruct_loss[imp_num] = evaluated_loss[i]
+    # print(impurity_anomaly_shape_scores)
+
+    # return impurity_anomaly_shape_reconstruct_loss
+    return impurity_anomaly_shape_scores
+
+
+if __name__ == "__main__":
+    test_2_impurities('./model_ae.h5')
+    # predict(path, impurities_num, model_name='./model.h5', height=600, width=600, BATCH_SIZE=64)
+
+
